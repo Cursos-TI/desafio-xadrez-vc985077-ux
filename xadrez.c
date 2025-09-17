5673ecff1657e6e687ebee70402e0f43014ff463
vc985077-ux <vc985077@gmail.com>
2025-09-17 01:39:50 +0000

    aventureiro

--- xadrez.c
+++ xadrez.c
@@ -2,32 +2,53 @@
 
 int main(){
     
-    int Torre = 0, Rainha = 0, Bispo = 0;
+    int Torre = 0, Rainha = 0, Bispo = 0, Cavalo = 0;
 
+    printf(" ** Torre **\n");
     while (Torre < 5) //Condição para Torre
     {
-        printf("\nTorre move-se 1 casa para a direita..."); //Saída Torre
+        printf("Direita\n"); //Saída Torre
 
         Torre++; //incremento de Torre
     }
+    printf("\n");
 
+    printf(" ** Bispo **\n");
     do
     {
-        printf("\nBispo move-se 1 casa para a direita..."); //Ação
+        printf("Direita\n"); //Ação
         
         Bispo++; //Incremento Rainha
 
         if (Bispo <= 5) //Condição if para soma
         {
-            printf("\nBispo move-se 1 casa para cima...");
+            printf("Cima");
         }
+        printf("\n");
         
     } while (Bispo < 5); // Saída while
+    printf("\n");
 
-    for ( Rainha = 0; Rainha < 8; Rainha++)
+    printf(" ** Rainha **\n"); 
+    for ( Rainha = 0; Rainha < 8; Rainha++) //Loop peça Rainha
     {
-        printf("\nRainha move-se 1 casa para a esquerda...");
+        printf("Esquerda\n");
     }
+    printf("\n");
+
+
+    printf(" ** Cavalo **\n");
+    for ( Cavalo = 0; Cavalo < 1; Cavalo++) //Loop externo Cavalo
+    {
+        while (Cavalo < 2) //Loop interno
+        {
+            printf("Baixo\n"); //Saída interna
+            Cavalo++;
+        }
+        
+        printf("Esquerda\n"); // Saída externa
+    }
+    
     
     return 0;
 }
